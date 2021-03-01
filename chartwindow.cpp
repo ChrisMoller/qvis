dd8b4710c8c8dd10989afb7cbcb0ba63d9164812
ChrisMoller <moller@mollerware.com>
2021-02-28 19:12:47 -0500

    cleanup

--- chartwindow.cpp
+++ chartwindow.cpp
@@ -661,28 +661,21 @@ ChartWindow::closeEvent (QCloseEvent *event __attribute__((unused)))
     if (widg == chartControls) {
       tabs->removeTab (i);
       cd = chartControls-> getChartData ();
-      fprintf (stderr, "cd = %p\n", cd);
       delete cd;
       delete chartControls;
       break;
     }
   }
-    QList<ChartData*> *charts = mw->getCharts ();
-#if 1
-  fprintf (stderr, "before %d\n", charts->size ());
+
   if (cd) {
+    QList<ChartData*> *charts = mw->getCharts ();
     for (i = 0; i < charts->size (); i++) {
-      fprintf (stderr, "chart %d %p\n", i, (*charts)[i]);
       if (cd == (*charts)[i]) {
 	charts->removeAt (i);
-	fprintf (stderr, "removing %d\n", i);
 	break;
       }
     }
   }
-  fprintf (stderr, "after %d\n", charts->size ());
-#endif
-  
 }
 
 ChartWindow::ChartWindow  (ChartControls *parent)
